8f06765dba5b309cb95d35498ce1fe37406b7f8e
Jan Wassermann <jan.wassermann@jansalleine.com>
2023-11-30 13:08:52 +0100

    New Directory Structure

--- Releases/Piepmatz/compile.sh
+++
@@ -1,5 +0,0 @@
-rm out-exo.prg;
-acme -v4 -f cbm -o out.prg music-player.asm;
-exomizer sfx 0x0800 -s "lda #\$0b sta \$d011" -x3 -o out-exo.prg out.prg;
-rm out.prg;
-x64sc -VICIIborders 0 out-exo.prg;
--- Releases/We Party Heart/compile.sh
+++
@@ -1,5 +0,0 @@
-rm -f wepartyheart.o;
-rm -f wepartyheart.prg;
-acme -v4 -f cbm -o wepartyheart.o wepartyheart.asm;
-exomizer sfx 0x0810 -x1 -o wepartyheart.prg wepartyheart.o;
-x64sc wepartyheart.prg;
